957ccc932138173442d2e14d5e8667dfac818579
Karen Etheridge <ether@cpan.org>
2017-08-26 17:30:31 -0700

    increment $VERSION after 0.001 release

--- lib/Dist/Zilla/PluginBundle/Git/VersionManager.pm
+++ lib/Dist/Zilla/PluginBundle/Git/VersionManager.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::PluginBundle::Git::VersionManager;
 # ABSTRACT: A plugin bundle that manages your version in git
 # KEYWORDS: bundle distribution git version Changes increment
 
-our $VERSION = '0.001';
+our $VERSION = '0.002';
 
 use Moose;
 with
